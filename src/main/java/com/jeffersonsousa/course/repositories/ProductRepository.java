65b1f93347098b36a0ebe4a3345c3471972bb3b9
Jefferson S Sousa <125930065+JeffSSousa@users.noreply.github.com>
2025-05-23 15:07:40 -0300

    Product

---
+++ src/main/java/com/jeffersonsousa/course/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.jeffersonsousa.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jeffersonsousa.course.entities.Product;
+
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+}
